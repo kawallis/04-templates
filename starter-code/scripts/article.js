1a2039502661dbbce23c4d7ebea3064d98bdf606
Chris_Wallace <kawallis@icloud.com>
2017-03-09 14:59:44 -0800

    made handlebar

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,7 +15,8 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -24,7 +25,8 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  var html = template(this);
+  return html;
 };
 
 rawData.sort(function(a,b) {
